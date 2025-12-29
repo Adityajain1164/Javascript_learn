7255f60de7ed88a5b10a8a4f695cbef218e31216
Adityajain1164 <2023uce1164@mnit.ac.in>
2025-12-29 12:55:49 +0000

    just cummit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1234;
+let accountEmail="adityajain1164@gmail.com";
+var accountPassword="11223344";
+accountCity="jaipur";
+let accountState;
+console.log(accountCity);
+
+//accountId=234
+//console.log(accountId);// this is not allowed in javascript
+accountEmail="2023uce1164@mnit.ac.in";
+accountPassword="1234";
+
+
+console.table([accountEmail, accountId,accountPassword,accountCity,accountState]);
+
+/*
+for defining constant always use const
+for variables always use let not var because var has issue with block scope and functional scope
+we can also define some thing without using these let and const but that is not good always try to define with let and conts 
+*/
+
