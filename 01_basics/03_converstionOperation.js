2013735791fba8456921639094fceebcb6400e29
Adityajain1164 <2023uce1164@mnit.ac.in>
2025-12-29 13:49:04 +0000

    learn converstion of datatypes and operations

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -1,14 +1,14 @@
 let score="33"
 let score_1="33abc"
 // some times data come form backend and u are working in fend so its imp to find its datatype
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 
 let scoreInNumbers = Number(score) // capital N
 let scoreInNumbers_1 = Number(score_1)
-console.log(typeof scoreInNumbers)
-console.log(typeof scoreInNumbers_1) // this will not show error it will show number
-console.log(scoreInNumbers_1)// this will show NAN
+//console.log(typeof scoreInNumbers)
+//console.log(typeof scoreInNumbers_1) // this will not show error it will show number
+//console.log(scoreInNumbers_1)// this will show NAN
 /*
 so in js never relai on Numbers() it can be NAN (not a number) always check the value
 null will convert into 0 ** very crusial and it will still convert into number
@@ -23,5 +23,46 @@ for boolean
 
 let isnum=33
 let isstring = String(isnum)
-console.log(typeof isstring)
-console.log(isstring)
+//console.log(typeof isstring)
+//console.log(isstring)
+
+
+
+// **************** Operations *****************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
