2c8ad8b6e3dc8e417f3f213984eb8bb9cf8dc9f5
Adityajain1164 <2023uce1164@mnit.ac.in>
2025-12-29 12:51:34 +0000

    Add JavaScript basics: variables and data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+// intial js did not has clases modules but newer verson has
+"use strict";
+// we use this line to treat all js code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+// in js we can or not use ';' and white spaces do not count
+
+console.log(3+
+    3
+) // this is also correct but readability is not good
+console.log("aditya"); // this is also ture
+
+// mdn and tc39 two basic documentation of js
+
+
+let age=18
+let isLoggedIn=false
+let state;
+
+/* number => 2 to power 53
+bigint
+string => ""
+boolean => true/false 
+null => standalone value
+undefine =>
+symbol => unique 
+
+
+object 
+*/
+
+console.log(typeof age)
+console.log(typeof null) // its type is showing object which is mistake of js
+console.log(typeof undefined)// its type is undefine 
