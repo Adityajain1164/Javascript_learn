a9974756c3de8549cb8b1f6f4ad7bd9fc8df1bb8
Adityajain1164 <2023uce1164@mnit.ac.in>
2025-12-30 08:55:01 +0000

    temp

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+// use the morden syntex of string 
+const name = "aditya"
+const repocount=50
+
+console.log(name + repocount+ " repos" )
+// never use above syntax for concatination 
+
+// always use backtricks ``
+conslo
