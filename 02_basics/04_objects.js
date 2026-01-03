14882b15651185022728dcb8322d3e23337b92de
Adityajain1164 <2023uce1164@mnit.ac.in>
2026-01-03 09:26:26 +0000

    objects and api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -72,3 +72,133 @@ console.log(tinderUser.hasOwnProperty('isLogged')) //* false
 
 
 //* other important method we can learn from browser console
+
+
+
+
+
+
+//* lets understand the destructuring of objects 
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructure:"hitesh"
+}
+//course.courseInstructure
+//* jab hame ek do baar chizo ka use karna pade 
+const {courseInstructure}=course
+// so from now we dont need to always mention course.blhablablah
+console.log(courseInstructure);
+//* if courseInstructure is a big name then we can use a nickname 
+const {courseInstructure:cI}=course
+console.log(cI);
+
+
+
+//* how we use this in react 
+const navbar =({company}) => { // insted of props.company
+    //* when ever you see this {} understand that is concept of dstructuring 
+
+
+}
+navbar(company="nehaorganics")
+
+
+
+
+
+
+
+
+//* lets learn something about api 
+//* when ever you put your work on someone else headache that is api
+//* apke backend se value ati hai aap un values ko kis tarah likhte hai 
+//* in past time these values come in the xml structure and that is very complex 
+//* in present time values come in json form 
+
+
+//* a json is nothing but a object jiska koi naam nhi hai 
+
+//{
+//    "name" : "aditya",
+//    "Id" : "a@gmail.com",
+//    "price":"free" 
+
+//}
+
+// we can also get the data in aaray
+
+[
+    {},
+    {},
+]
+
+//* randomuser me api example
+//* we have different tools to understand this data jason formatter
+
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
