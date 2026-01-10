9aee4edeccd93ce3f36beb3d337b924c1c2b776c
Aditya Jain <2023uce1164@mnit.ac.in>
2026-01-10 12:21:17 +0000

    while loop

---
+++ 05_iteration/02_while.js
@@ -0,0 +1,26 @@
+/// while 
+
+let i=0;
+while(i<=10){
+    console.log(i);
+    i=i+2
+    
+}
+let myarray = ["flash","batman","superman"]
+let arr =0;
+while(arr<myarray.length){
+    console.log(`value is ${myarray[arr]}`);
+    arr++
+}
+
+//* do while loop
+//* this loop is very rarely used in some special cases
+//* first code is executed then condition is checked 
+//let score =1 //*output will be one to ten 
+let score=11 //* it will not give you error output will be 11
+do {
+    console.log(score);
+    score++
+    
+} while (score<=10);
+
